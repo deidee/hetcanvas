a773b9644f889c96713542c484598af1c6033609
acj <gitlab@acjs.net>
2019-03-05 11:49:55 +0100

    TODO.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var twig = require('gulp-twig');
 gulp.task('templates', function() {
     return gulp.src('src/*.twig') // run the Twig template parser on all .html files in the "src" directory
         .pipe(twig({
+            // TODO: Feed it JSON, or something.
             data: {
                 artwork: {
                     dir: 'ltr',
