f9b8b861822df17e8ee1917b455ffa77ad1024d9
acj <gitlab@acjs.net>
2019-03-05 11:49:23 +0100

    Feed it data.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,18 @@ var twig = require('gulp-twig');
 // Compile Twig templates to HTML
 gulp.task('templates', function() {
     return gulp.src('src/*.twig') // run the Twig template parser on all .html files in the "src" directory
-        .pipe(twig())
+        .pipe(twig({
+            data: {
+                artwork: {
+                    dir: 'ltr',
+                    lang: 'en',
+                    description: 'Blank canvas.',
+                    title: 'canvas',
+                    themeColor: '#a00ade',
+                    image: '',
+                    url: '/'
+                }
+            }
+        }))
         .pipe(gulp.dest('dist')); // output the rendered HTML files to the "dist" directory
 });
